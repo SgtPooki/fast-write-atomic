62da2a01a99d9f0010a98cdc418d9c604d543e7d
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-12-14 12:44:47 -0800

    test: add steno to benchmark

---
+++ bench.mjs
@@ -0,0 +1,24 @@
+import bench from 'fastbench'
+import { tmpdir } from 'os'
+import { join } from 'path'
+import writeFileAtomic from 'write-file-atomic'
+import fastWriteAtomic from './index.js'
+import { Writer as stenoWriter } from 'steno'
+
+const dest = join(tmpdir(), 'dest')
+const file = Buffer.allocUnsafe(1024 * 1024) // 1MB
+
+const run = bench([
+  function benchWriteFileAtomic (cb) {
+    writeFileAtomic(dest, file, cb)
+  },
+  function benchFastWriteAtomic (cb) {
+    fastWriteAtomic(dest, file, cb)
+  },
+  function benchStenoWrite (cb) {
+    const writer = new stenoWriter(dest)
+    writer.write(file).then(cb, cb)
+  }
+], 1000)
+
+run(run)
